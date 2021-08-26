cdc3abc16244b9748457c7b93fe17c30332cc549
FadeevDV <dfadeev.zld@yandex.ru>
2021-08-26 21:14:54 +0300

    Comm_final_rew1,4

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from django.conf import settings
 from rest_framework import serializers
 
 from recipes.models import Recipe
